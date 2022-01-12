1f1c59d84f496b82a17e943be2b23524534922ca
Christopher Slinker <christopher.slinker@gmail.com>
2022-01-12 10:11:48 -0700

    Messing around with inheritance some more

--- In Class/In Class/Assets/Scripts/Inheritance/Cylinder.cs
+++ In Class/In Class/Assets/Scripts/Inheritance/Cylinder.cs
@@ -1,8 +1,12 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Cylinder : CylinderBase
 {
-
+    private void FixedUpdate()
+    {
+        transform.Translate(.1f, 0, 0);
+    }
 }
