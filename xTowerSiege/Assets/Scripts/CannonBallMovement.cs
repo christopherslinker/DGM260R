091ffb6603af88f7c3e0e8112c734dc89d5fc496
Christopher Slinker <christopher.slinker@gmail.com>
2022-01-14 15:26:26 -0700

    Cannon ball prefab and movement made

---
+++ xTowerSiege/Assets/Scripts/CannonBallMovement.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonBallMovement : MonoBehaviour
+{
+    public float speed = 100f;
+    public GameObject player;
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
