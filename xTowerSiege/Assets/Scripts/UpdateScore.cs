6cc0477a7d18b66f406e79c45fd2af90a443855e
Christopher Slinker <christopher.slinker@gmail.com>
2022-02-07 14:15:47 -0700

    Working on Enemy dying updating score.

---
+++ xTowerSiege/Assets/Scripts/UpdateScore.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UpdateScore : MonoBehaviour
+{
+    private ScoreManager scoreManager;
+
+
+    void Start()
+    {
+        scoreManager = GameObject.Find("ScoreManager").GetComponent<ScoreManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ammo"))
+        {
+            scoreManager.UpdateScore(5);
+        }
+    }
+}
