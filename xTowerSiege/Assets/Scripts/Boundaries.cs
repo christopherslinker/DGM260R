bdad952dbda673a16c0b8982e293cd0f216b199a
Christopher Slinker <christopher.slinker@gmail.com>
2022-02-12 19:02:04 -0700

    Working on invisible boundaries

---
+++ xTowerSiege/Assets/Scripts/Boundaries.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boundaries : MonoBehaviour
+{
+    private float horizontalInput;
+
+    private float verticalInput;
+
+    private float speed = 5.0f;
+
+    private float xLowRange = -605f;
+
+    private float xHighRange = 870f;
+
+    private float zRange = 8.5f;
+    
+    
+    void Update()
+    {
+        if (transform.position.x < xLowRange)
+        {
+            transform.position = new Vector3(-xLowRange, transform.position.y, transform.position.z);
+        }
+        
+        if (transform.position.x > xHighRange)
+        {
+            transform.position = new Vector3(xHighRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.z < -zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, -zRange );
+        }
+        
+        if (transform.position.z > zRange)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y, zRange);
+        }
+    }
+}
