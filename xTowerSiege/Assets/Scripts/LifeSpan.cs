4208dc799ade182c70aaea86894196b2e74b4700
Christopher Slinker <christopher.slinker@gmail.com>
2022-04-04 15:05:18 -0600

    Gems destroy after a set amount of time.

--- xTowerSiege/Assets/Scripts/LifeSpan.cs
+++ xTowerSiege/Assets/Scripts/LifeSpan.cs
@@ -7,14 +7,9 @@ public class LifeSpan : MonoBehaviour
 
     public float lifeSpan = 2.0f;
 
-
     // Update is called once per frame
     void Update()
     {
-        if (Time.deltaTime > lifeSpan)
-        {
-            Destroy(gameObject);
-        }
-        
+        Destroy(gameObject, lifeSpan);
     }
 }
